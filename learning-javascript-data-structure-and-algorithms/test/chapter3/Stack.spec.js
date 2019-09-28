dfc6ef461db45e516db102d77b981e23161004b9
amnhh <2570079254@qq.com>
2019-09-28 10:13:20 +0800

    [test case] stack test case adden

--- learning-javascript-data-structure-and-algorithms/test/chapter3/Stack.spec.js
+++ learning-javascript-data-structure-and-algorithms/test/chapter3/Stack.spec.js
@@ -6,9 +6,7 @@ const logger = console.log
 
 const Stack = require('../../chapter3/Stack')
 
-logger(prefix)
-
-test(`${prefix}Stack pop and push`, t => {
+test(`${prefix}Stack pop and push methods`, t => {
     const st = new Stack()
     // size equals 0 when init.
     t.is(st.size(), 0)
@@ -25,3 +23,33 @@ test(`${prefix}Stack pop and push`, t => {
     t.is(stItem, '2')
     t.is(st.size(), 1)
 });
+
+test(`${prefix}Stack clear method`, t => {
+    const st = new Stack()
+    st.push(1)
+    st.push('2')
+
+    t.is(st.size(), 2)
+
+    st.clear()
+    t.is(st.size(), 0)
+})
+
+test(`${prefix}Stack isEmpty method`, t => {
+    const st = new Stack()
+    t.true(st.isEmpty())
+
+    st.push(5)
+    t.false(st.isEmpty())
+})
+
+test(`${prefix}Stack peek method`, t => {
+    const st = new Stack()
+    t.is(st.peek(), void 0)
+
+    st.push(5)
+    t.is(st.peek(), 5)
+
+    st.push(9, 1)
+    t.is(st.peek(), 1)
+})
